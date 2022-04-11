974ff945efcf637bec93d2a3b3b70022813bee59
Sachin Kumar Keshri <helpmeskk@gmail.com>
2022-04-11 12:56:04 +0530

    sachin

--- src/components/Grocerydetails.jsx
+++ src/components/Grocerydetails.jsx
@@ -5,11 +5,34 @@ import data from '../data.json'
 const GroceryDetails = ()=>{
 console.log(data)
 
+const appendData=(data)=>{
+
+    console.log("hello i am working")
+    for(var i=0; i<10; i++)
+    {
+       let div =document.createElement("div");
+       let name=document.createElement("p");
+       name.innerHTML=data[i].title;
+     
+        let p =document.createElement("p");
+        p.innerText=data[i].price;
+
+    document.createElement("p").innerText=data[i].title
+        
+       
+    }
+    
+}
+
+
+
     return(
         <>
         <h1>Groceries</h1>
         <div className="container">
             {/* map through the data and display the cards */}
+       
+        {/* { appendData(data)} */}
         </div>
         </>
     )
